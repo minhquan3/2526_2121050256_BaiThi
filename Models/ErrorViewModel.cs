d255bd848078ab5ef32d7bddb37695bbeaa3ecaf
minhquan3 <Your_Email>
2026-06-19 20:56:29 +0700

    Create_project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2526_2121050111_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
